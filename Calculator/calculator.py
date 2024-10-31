737d2c2abac0c19ec0cb4950466f2312ffc069e6
Nikou <Poppy.Nikou@uclh.nhs.uk>
2024-10-31 12:09:30 +0000

    intial commit

---
+++ Calculator/calculator.py
@@ -0,0 +1,33 @@
+import numpy as np 
+
+def addition(x,y):
+    """Adds x and y. 
+
+    :param x: Single number x
+    :type x: int or float
+    :param y: Single number y 
+    :type y: int or float
+    :return: Sum of x and y
+    :rtype: int or float
+
+    Examples:
+
+    >>> addition(1,3)
+    4
+
+    >>> addition('str', 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Input must be an integer or float
+
+    """
+
+    # Ensure x is int or float
+    if type(x) != int or type(x) != float:
+        raise ValueError("Input must be an integer or float")
+
+    # Ensure y is int or float
+    if type(y) != int or type(y) != float:
+        raise ValueError("Input must be an integer or float")
+
+    return x+y 
---
+++ Calculator/test/test_calculator.py
@@ -0,0 +1,16 @@
+from .test_calculator import addition
+from pytest import raises, mark
+
+
+
+# testing functionality of addition
+def test_addition():
+
+    result = addition(2,3)
+    assert result ==5 
+
+# test wrong input
+def test_addition_input():
+
+    with raises(ValueError, match ='Input must be an integer or float'):
+        addition('s', 3)
